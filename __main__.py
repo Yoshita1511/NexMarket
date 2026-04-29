fa376b12625a6743fd90efb288b30b1c1b0819bf
Yoshita1511 <yoshita.bora15@gmail.com>
2026-04-29 17:33:17 +0530

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,3 +1,17 @@
-from .cli import main
+"""
+    pygments.__main__
+    ~~~~~~~~~~~~~~~~~
 
-main()
+    Main entry point for ``python -m pygments``.
+
+    :copyright: Copyright 2006-2024 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+import sys
+from pip._vendor.pygments.cmdline import main
+
+try:
+    sys.exit(main(sys.argv))
+except KeyboardInterrupt:
+    sys.exit(1)
